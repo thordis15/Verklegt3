f31bbc33157edc16dd57321b55c72d62d680c66d
Siggnja <Siggnja@gmail.com>
2015-12-11 11:31:59 +0000

    search komid

--- build-vika3-Desktop_Qt_5_5_1_MinGW_32bit-Debug/debug/debug/moc_mainwindow.cpp
+++ build-vika3-Desktop_Qt_5_5_1_MinGW_32bit-Debug/debug/debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[9];
-    char stringdata0[204];
+    QByteArrayData data[11];
+    char stringdata0[255];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,14 +37,18 @@ QT_MOC_LITERAL(4, 45, 33), // "on_lineEdit_searchSci_textCha..."
 QT_MOC_LITERAL(5, 79, 4), // "arg1"
 QT_MOC_LITERAL(6, 84, 41), // "on_comboBox_searchSci_current..."
 QT_MOC_LITERAL(7, 126, 34), // "on_lineEdit_searchComp_textCh..."
-QT_MOC_LITERAL(8, 161, 42) // "on_comboBox_searchComp_curren..."
+QT_MOC_LITERAL(8, 161, 42), // "on_comboBox_searchComp_curren..."
+QT_MOC_LITERAL(9, 204, 24), // "on_Button_addSci_clicked"
+QT_MOC_LITERAL(10, 229, 25) // "on_Button_editSci_clicked"
 
     },
     "MainWindow\0on_tabWidget_tabBarClicked\0"
     "\0index\0on_lineEdit_searchSci_textChanged\0"
     "arg1\0on_comboBox_searchSci_currentIndexChanged\0"
     "on_lineEdit_searchComp_textChanged\0"
-    "on_comboBox_searchComp_currentIndexChanged"
+    "on_comboBox_searchComp_currentIndexChanged\0"
+    "on_Button_addSci_clicked\0"
+    "on_Button_editSci_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -54,7 +58,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -62,11 +66,13 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   39,    2, 0x08 /* Private */,
-       4,    1,   42,    2, 0x08 /* Private */,
-       6,    1,   45,    2, 0x08 /* Private */,
-       7,    1,   48,    2, 0x08 /* Private */,
-       8,    1,   51,    2, 0x08 /* Private */,
+       1,    1,   49,    2, 0x08 /* Private */,
+       4,    1,   52,    2, 0x08 /* Private */,
+       6,    1,   55,    2, 0x08 /* Private */,
+       7,    1,   58,    2, 0x08 /* Private */,
+       8,    1,   61,    2, 0x08 /* Private */,
+       9,    0,   64,    2, 0x08 /* Private */,
+      10,    0,   65,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Int,    3,
@@ -74,6 +80,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::QString,    5,
     QMetaType::Void, QMetaType::QString,    5,
     QMetaType::Void, QMetaType::QString,    5,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -89,6 +97,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_comboBox_searchSci_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         case 3: _t->on_lineEdit_searchComp_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         case 4: _t->on_comboBox_searchComp_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 5: _t->on_Button_addSci_clicked(); break;
+        case 6: _t->on_Button_editSci_clicked(); break;
         default: ;
         }
     }
@@ -119,13 +129,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 7;
     }
     return _id;
 }
